e85120e6f1a7d4f1dea71e1dfaf3e170f27bc42f
salaboy <salaboy@gmail.com>
2019-08-03 11:17:24 +0100

    adding version to chart and app

--- src/main/java/com/salaboy/conferences/conferenceagenda/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/conferenceagenda/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.salaboy.conferences.conferenceagenda;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Value("${version:0}")
+    private String version;
+
+    @GetMapping
+    public String helloVersion() {
+        return "Hello human, my version is: " + version;
+    }
 
 }
